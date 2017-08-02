b58ef0d8852c3df22cc9edccbcad35c035e2b7a0
David Cornelius <davidcornelius15@gmail.com>
2017-08-02 21:08:53 +0700

    Scroll View Demo

--- Scroll View Demo/ViewController.swift
+++ Scroll View Demo/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBAction func hello(_ sender: Any)
+    {
+        print("HELLO")
+        
+    }
+    
+    
+    @IBAction func world(_ sender: Any)
+    {
+        print("WORLD")
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
